845748a29031172d045817e21cf0109589441356
ckak912 <ckak912@aucklanduni.ac.nz>
2023-10-02 21:40:31 +1300

    changed migration script

--- front_end/migration_scripts/46_to_47_migrate.sql
+++ front_end/migration_scripts/46_to_47_migrate.sql
@@ -1,7 +1,5 @@
--- Add the new column with a default value
 ALTER TABLE LLM_stuff
-ADD COLUMN date_created timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ADD COLUMN date_created timestamp NOT NULL;
 
--- Update the new column with values from presubmissions
 UPDATE LLM_stuff
 SET date_created = (SELECT date_updated FROM presubmissions WHERE LLM_stuff.date_created == presubmissions.date_updated);
