572a55f42948066c654070085a7a588aaa04bce5
ckak912 <ckak912@aucklanduni.ac.nz>
2023-10-02 21:43:10 +1300

    losing willpower

--- front_end/migration_scripts/46_to_47_migrate.sql
+++ front_end/migration_scripts/46_to_47_migrate.sql
@@ -1,5 +1,5 @@
 ALTER TABLE LLM_stuff
-ADD COLUMN date_created timestamp NOT NULL;
+ADD COLUMN date_created timestamp NOT NULL DEFAULT 0;
 
 UPDATE LLM_stuff
 SET date_created = (SELECT date_updated FROM presubmissions WHERE LLM_stuff.date_created == presubmissions.date_updated);
